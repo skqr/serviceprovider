7a90fc8a29a126fecaed4d55edcb466cd82c94b1
Seba <devsebas@gmail.com>
2019-03-13 14:30:01 -0300

    keep original name

--- pyrovider/meta/tests/test_ioc.py
+++ pyrovider/meta/tests/test_ioc.py
@@ -7,11 +7,11 @@ class ImporterTest(unittest.TestCase):
 
     maxDiff = None
 
-    def test_get_obj(self):
+    def test_get_class(self):
         importer = Importer()
         self.assertEqual(Importer, importer.get_class('pyrovider.meta.ioc.Importer'))
 
-    def test_get_obj_undefined(self):
+    def test_get_class_undefined(self):
         importer = Importer()
         with self.assertRaises(KeyError) as context:
             importer.get_class('pyrovider.meta.ioc.Undefined')
